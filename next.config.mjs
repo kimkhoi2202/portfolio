f66ceba7741bf94b1aa47e712318bacc4919fc9c
Kim Khoi Lam <kimkhoi2202@gmail.com>
2026-05-17 15:15:44 -0700

    Update resume PDF

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/resume',
-        destination: '/Khoi Lam Resume.pdf',
+        destination: '/Khoi-Lam-Resume.pdf',
         permanent: true,
       },
     ];
